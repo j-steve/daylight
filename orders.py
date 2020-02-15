bf460db9efe8f1a79ece37b5441d8565b58617a6
Steve <steve5805@gmail.com>
2020-02-15 08:32:46 +0000

    Cleaned up order logic to be based on executions and exclude cancelled/unfilled orders.

--- orders.py
+++ orders.py
@@ -3,22 +3,22 @@ from decimal import Decimal
 
 def parse(orders):
   for order in orders:
+    if order['state'] != 'filled':
+      continue
     instrument = robin_stocks.helper.request_get(order['instrument'])
-    yield Order(order, instrument)
+    for execution in order['executions']:
+      yield Order(order, execution, instrument)
 
 class Order(object):
-  def __init__(self, order, instrument):
+  def __init__(self, order, execution, instrument):
     self.id = order['id']
     self.symbol = instrument['symbol']
-    self.quantity = float(order['cumulative_quantity'] or 0)
-    if order['executed_notional']:
-      self.total_price = float(order['executed_notional']['amount'] or 0)
-    else:
-      self.total_price = 0
-    self.share_price = self.total_price / self.quantity if self.quantity else None
+    self.quantity = float(execution['quantity'])
+    self.share_price = float(execution['price'])
+    self.total_price = self.quantity * self.share_price
     self.fees = float(order['fees'] or 0)
     self.type = order['side'] # buy or sell
-    self.timestamp = order['executions'] and order['executions'][0]['timestamp'] or None
+    self.timestamp = execution['timestamp'] or None
     if self.type == 'sell':
       self.fees_computed = 0.00002 * self.total_price + 0.000119 * self.quantity + 0.02
       self.fees_computed = round(self.fees_computed, 2)
