803acfdac4976a79788210a07763c6e2e3898340
Steve <steve5805@gmail.com>
2020-02-15 01:03:43 +0000

    Adds full flow from login to SMS to data.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 app = Flask('app')
 
 import robin_stocks
@@ -11,8 +11,25 @@ import robin_stocks
 def login():
   return render_template('login.html')
 
+@app.route('/login', methods=['POST'])
+def login_submit():
+  return redirect(url_for('sms_code'))
+
+@app.route('/sms-code')
+def sms_code():
+  return render_template('sms_code.html')
+
+@app.route('/sms-code', methods=['POST'])
+def sms_cod_submit():
+  return redirect(url_for('data'))
+
+@app.route('/data')
+def data():
+  return render_template('data.html', transactions = [{"price": 5}])
+
 @app.route('/')
 def hello_world():
+  return redirect(url_for('login'))
   # result = '<table><tr><th>Price</th><th>Quantity</th><th>Date</th></tr>'
   # for o in orders:
   #   result += '<tr><td>{price}</td><td>{quantity}</td><td>{date}</td></tr>'.format(price=o.get('price', ''), quantity=o.get('quantity', ''), date=o.get('last_transaction_at', ''))
