1e2e9d17a2760010a6e1ec117270ae1aa41af248
Steve <steve5805@gmail.com>
2020-02-17 20:28:15 +0000

    Removes unused reference.

--- orders.py
+++ orders.py
@@ -1,24 +1,41 @@
 import robin_stocks
-from decimal import Decimal
+from collections import defaultdict
 
-def parse(orders):
-  for order in orders:
+def parse(order_data):
+  orders = []
+  for order in order_data:
     if order['state'] != 'filled':
       continue
     instrument = robin_stocks.helper.request_get(order['instrument'])
     for execution in order['executions']:
-      yield Order(order, execution, instrument)
+      orders.append(Order(order, execution, instrument))
+  # Compute profit/loss for each sale.
+  orders.sort(key=lambda x: x.timestamp)
+  transactions = defaultdict(lambda: [])
+  for o in orders:
+    if o.type == 'buy':
+      for i in range(o.quantity):
+        transactions[o.symbol].append(o.share_price)
+    elif o.type == 'sell':
+      o.profit = o.total_price - o.fees
+      for i in range(o.quantity):
+        if transactions[o.symbol]:
+          # May be no buy transaction, if this was 
+          # a free robinhood stock.
+          o.profit -= transactions[o.symbol].pop(-1)
+  return orders
 
 class Order(object):
   def __init__(self, order, execution, instrument):
     self.id = order['id']
     self.symbol = instrument['symbol']
-    self.quantity = float(execution['quantity'])
+    self.quantity = int(float(execution['quantity']))
     self.share_price = float(execution['price'])
     self.total_price = self.quantity * self.share_price
-    self.fees = float(order['fees'] or 0)
+    self.fees = float(order['fees']) / int(float(order['quantity']))
     self.type = order['side'] # buy or sell
     self.timestamp = execution['timestamp'] or None
     if self.type == 'sell':
       self.fees_computed = 0.00002 * self.total_price + 0.000119 * self.quantity + 0.02
       self.fees_computed = round(self.fees_computed, 2)
+    self.profit = None
